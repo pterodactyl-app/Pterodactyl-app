1d8e3e2b5af40f0de3de7cc3248d6d1ab79cfd13
Arnaud Lier <zeprofdecoding@gmail.com>
2019-04-29 18:50:39 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,103 +1,135 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'dart:async';
-import 'dart:convert';
-
-import 'login.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'home.dart';
-import 'servers.dart';
-import 'settings.dart';
-import 'about.dart';
+import 'main.dart';
 
-class DemoLocalizations {
-  DemoLocalizations(this.locale);
-
-  final Locale locale;
-
-  static DemoLocalizations of(BuildContext context) {
-    return Localizations.of<DemoLocalizations>(context, DemoLocalizations);
-  }
-
-  Map<String, String> _sentences;
-
-  Future<bool> load() async {
-    String data = await rootBundle
-        .loadString('assets/lang/${this.locale.languageCode}.json');
-    Map<String, dynamic> _result = json.decode(data);
-
-    this._sentences = new Map();
-    _result.forEach((String key, dynamic value) {
-      this._sentences[key] = value.toString();
-    });
-
-    return true;
-  }
-
-  String trans(String key) {
-    return this._sentences[key];
-  }
+class User {
+  final String api, url;
+  const User({
+    this.api,
+    this.url,
+  });
 }
 
-class DemoLocalizationsDelegate
-    extends LocalizationsDelegate<DemoLocalizations> {
-  const DemoLocalizationsDelegate();
-
+class LoginPage extends StatefulWidget {
   @override
-  bool isSupported(Locale locale) =>
-      ['en', 'fr', 'nl', 'de'].contains(locale.languageCode);
-
-  @override
-  Future<DemoLocalizations> load(Locale locale) async {
-    DemoLocalizations localizations = new DemoLocalizations(locale);
-    await localizations.load();
-
-    print("Load ${locale.languageCode}");
-
-    return localizations;
-  }
-
-  @override
-  bool shouldReload(DemoLocalizationsDelegate old) => false;
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class MyApp extends StatelessWidget {
+class _LoginPageState extends State<LoginPage> {
+  final _apiController = TextEditingController();
+  final _urlController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-        supportedLocales: [
-          const Locale('en', 'US'),
-          const Locale('fr', 'FR'),
-          const Locale('nl', 'NL')
-        ],
-        localizationsDelegates: [
-          const DemoLocalizationsDelegate(),
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate
-        ],
-        localeResolutionCallback:
-            (Locale locale, Iterable<Locale> supportedLocales) {
-          for (Locale supportedLocale in supportedLocales) {
-            if (supportedLocale.languageCode == locale.languageCode ||
-                supportedLocale.countryCode == locale.countryCode) {
-              return supportedLocale;
-            }
-          }
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            SizedBox(height: 80.0),
+            Column(
+              children: <Widget>[
+                Image.asset('assets/images/pterodactyl_icon.png', width: 100),
+                SizedBox(height: 8.0),
+                Text(
+                  'PTERODACTYL APP',
+                  style: Theme.of(context).textTheme.headline,
+                ),
+              ],
+            ),
+            SizedBox(height: 80.0),
+            AccentColorOverride(
+              color: Color(0xFF442B2D),
+              child: TextField(
+                controller: _apiController,
+                decoration: InputDecoration(
+                  labelText:
+                      DemoLocalizations.of(context).trans('api_key_login'),
+                ),
+              ),
+            ),
+            SizedBox(height: 12.0),
+            AccentColorOverride(
+              color: Color(0xFFC5032B),
+              child: TextField(
+                controller: _urlController,
+                decoration: InputDecoration(
+                  labelText: DemoLocalizations.of(context).trans('url_login'),
+                ),
+              ),
+            ),
+            ButtonBar(
+              children: <Widget>[
+                FlatButton(
+                  child: Text(
+                    DemoLocalizations.of(context).trans('clear_login'),
+                  ),
+                  shape: BeveledRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                  ),
+                  onPressed: () {
+                    _apiController.clear();
+                    _urlController.clear();
+                  },
+                ),
+                RaisedButton(
+                  child: Text(
+                    DemoLocalizations.of(context).trans('next_login'),
+                  ),
+                  elevation: 8.0,
+                  shape: BeveledRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                  ),
+                  onPressed: () {
+                    var route = new MaterialPageRoute(
+                      builder: (BuildContext context) => new MyHomePage(
+                          value: User(
+                              //api: _read("apikey"),
+                              //url: _read("panelurl"))),
+                              api: _apiController.text,
+                              url: _urlController.text)),
+                    );
+                    Navigator.of(context).push(route);
+                    _save("apikey", _apiController.text);
+                    _save("panelurl", _apiController.text);
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
-          return supportedLocales.first;
-        },
-        title: 'PTERODACTYL APP',
-        home: new LoginPage(),
-        routes: <String, WidgetBuilder>{
-          '/login': (BuildContext context) => new LoginPage(),
-          '/home': (BuildContext context) => new MyHomePage(),
-          '/servers': (BuildContext context) => new ServerListPage(),
-          '/about': (BuildContext context) => new AboutPage(),
-          '/settings': (BuildContext context) => new SettingsList(),
-        });
+  _save(String key, String value) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString(key, value);
+  }
+
+  _read(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    final value = prefs.getString('key') ?? "";
+    return value;
   }
 }
 
-void main() {
-  runApp(new MyApp());
+class AccentColorOverride extends StatelessWidget {
+  const AccentColorOverride({Key key, this.color, this.child})
+      : super(key: key);
+
+  final Color color;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      child: child,
+      data: Theme.of(context).copyWith(
+        accentColor: color,
+        brightness: Brightness.dark,
+      ),
+    );
+  }
 }
