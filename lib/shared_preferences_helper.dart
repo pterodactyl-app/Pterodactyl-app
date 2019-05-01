56ccdfea73c5054ea0482abec7fa9f118c33870a
Arnaud Lier <zeprofdecoding@gmail.com>
2019-05-01 09:36:48 +0200

    Update shared_preferences_helper.dart

--- lib/shared_preferences_helper.dart
+++ lib/shared_preferences_helper.dart
@@ -1,13 +1,13 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPreferencesHelper {
-  static Future<bool> setApiUrlString(String key, String value) async {
+  static Future<bool> setString(String key, String value) async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
 
     return prefs.setString(key, value);
   }
 
-  static Future<String> getApiUrlString(String key) async {
+  static Future<String> getString(String key) async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
 
     return prefs.getString(key) ?? '';
