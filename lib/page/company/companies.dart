d49b098381c927898217aca9bcb6585ebaa3cda2
Yvan E. Watchman <ywatchman@cyberfusion.nl>
2019-06-16 18:30:23 +0200

    Routing

--- lib/page/company/companies.dart
+++ lib/page/company/companies.dart
@@ -15,32 +15,70 @@
 */
 
 import 'package:flutter/material.dart';
-import 'package:pterodactyl_app/page/company/deploys/client/home.dart';
-import 'package:pterodactyl_app/page/company/coderslight/client/home.dart';
-import 'package:pterodactyl_app/page/company/minicenter/client/home.dart';
-import 'package:pterodactyl_app/page/company/planetnode/client/home.dart';
-import 'package:pterodactyl_app/page/company/revivenode/client/home.dart';
+import 'package:pterodactyl_app/about.dart';
 
+// CodersLight
+import 'package:pterodactyl_app/page/company/coderslight/client/home.dart';
+import 'package:pterodactyl_app/page/company/coderslight/client/login.dart';
+import 'package:pterodactyl_app/page/company/coderslight/client/servers.dart';
+import 'package:pterodactyl_app/page/company/coderslight/client/settings.dart';
+
+// Deploys
+import 'package:pterodactyl_app/page/company/deploys/client/home.dart';
+import 'package:pterodactyl_app/page/company/deploys/client/login.dart';
+import 'package:pterodactyl_app/page/company/deploys/client/servers.dart';
+import 'package:pterodactyl_app/page/company/deploys/client/settings.dart';
+
+/// Dart has no functionality to dynamically create class instances
+/// Till then, we'll have to manually add every route.
 Map companyRoutes() {
   Map c = <String, Map>{};
-  companies().forEach((f) => {
-        c[f] = <String, WidgetBuilder>{
-          '/' + f + '/home': (BuildContext context) => new MyDeploysHomePage(), // Todo: https://stackoverflow.com/a/14705238
-          '/' + f + '/home': (BuildContext context) => new MyCodersLightHomePage(),
-          '/' + f + '/home': (BuildContext context) => new MyMiniCenterHomePage(),
-          '/' + f + '/home': (BuildContext context) => new MyPlanetNodeHomePage(),
-          '/' + f + '/home': (BuildContext context) => new MyReviveNodeHomePage(),
-        }
-      });
+  c['coderslight'] = <String, WidgetBuilder>{
+    '/coderslight/home': (BuildContext context) => new MyCodersLightHomePage(),
+    '/coderslight/login': (BuildContext context) => new LoginCodersLightPage(),
+    '/coderslight/servers': (BuildContext context) => new CodersLightServerListPage(),
+    '/coderslight/about': (BuildContext context) => new AboutPage(),
+    '/coderslight/settings': (BuildContext context) => new CodersLightSettingsList(),
+  };
+  c['deploys'] = <String, WidgetBuilder>{
+    '/deploys/home': (BuildContext context) => new MyDeploysHomePage(),
+    '/deploys/login': (BuildContext context) => new LoginDeploysPage(),
+    '/deploys/servers': (BuildContext context) => new DeploysServerListPage(),
+    '/deploys/about': (BuildContext context) => new AboutPage(),
+    '/deploys/settings': (BuildContext context) => new DeploysSettingsList(),
+  };
+  c['minicenter'] = <String, WidgetBuilder>{
+    '/minicenter/home': (BuildContext context) => new MyDeploysHomePage(),
+    '/minicenter/login': (BuildContext context) => new LoginDeploysPage(),
+    '/minicenter/servers': (BuildContext context) => new DeploysServerListPage(),
+    '/minicenter/about': (BuildContext context) => new AboutPage(),
+    '/minicenter/settings': (BuildContext context) => new DeploysSettingsList(),
+  };
+  c['planetnode'] = <String, WidgetBuilder>{
+    '/planetnode/home': (BuildContext context) => new MyDeploysHomePage(),
+    '/planetnode/login': (BuildContext context) => new LoginDeploysPage(),
+    '/planetnode/servers': (BuildContext context) => new DeploysServerListPage(),
+    '/planetnode/about': (BuildContext context) => new AboutPage(),
+    '/planetnode/settings': (BuildContext context) => new DeploysSettingsList(),
+  };
+  c['revivenode'] = <String, WidgetBuilder>{
+    '/revivenode/home': (BuildContext context) => new MyDeploysHomePage(),
+    '/revivenode/login': (BuildContext context) => new LoginDeploysPage(),
+    '/revivenode/servers': (BuildContext context) => new DeploysServerListPage(),
+    '/revivenode/about': (BuildContext context) => new AboutPage(),
+    '/revivenode/settings': (BuildContext context) => new DeploysSettingsList(),
+  };
   return c;
 }
 
+//'/coderslight/home': (BuildContext context) => new MyCodersLightHomePage(),
+//'/minicenter/home': (BuildContext context) => new MyMiniCenterHomePage(),
+//'/planetnode/home': (BuildContext context) => new MyPlanetNodeHomePage(),
+//'/revivenode/home': (BuildContext context) => new MyReviveNodeHomePage(),
+
 List<String> companies() {
   List<String> companies = [];
-  companies.add('deploys');
-  companies.add('coderslight');
-  companies.add('minicenter');
-  companies.add('planetnode');
-  companies.add('revivenode');
+  companies.addAll(
+      ['deploys', 'coderslight', 'minicenter', 'planetnode', 'revivenode']);
   return companies;
 }
