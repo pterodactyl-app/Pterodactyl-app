ad4ddd8c2e41d400d377478e287092568efd52fc
rubentalstra <RubenTalstra1211@outlook.com>
2019-06-24 16:04:04 -0300

    icon update

--- lib/page/client/settings.dart
+++ lib/page/client/settings.dart
@@ -283,16 +283,16 @@ class Settings extends StatelessWidget {
       style: settingsStyle,
       child: Column(
         children: <Widget>[
-          SettingsItem(Icons.notifications, '#5FD0D3', DemoLocalizations.of(context).trans('notifications'),
+          SettingsItem(FontAwesomeIcons.bell, '#5FD0D3', DemoLocalizations.of(context).trans('notifications'),
               DemoLocalizations.of(context).trans('notifications_sub')),
-          SettingsItem(Icons.question_answer, '#BFACAA', DemoLocalizations.of(context).trans('dark_mode'),
+          SettingsItem(FontAwesomeIcons.adjust, '#BFACAA', DemoLocalizations.of(context).trans('dark_mode'),
               DemoLocalizations.of(context).trans('dark_mode_sub')),
 
 
           SettingsItem(
-              Icons.lock, '#F468B7', DemoLocalizations.of(context).trans('logout'), DemoLocalizations.of(context).trans('logout_sub')),
+              FontAwesomeIcons.signOutAlt, '#F468B7', DemoLocalizations.of(context).trans('logout'), DemoLocalizations.of(context).trans('logout_sub')),
           SettingsItem(
-              Icons.menu, '#FEC85C', DemoLocalizations.of(context).trans('delete_data'), DemoLocalizations.of(context).trans('delete_data_sub')),
+              FontAwesomeIcons.trashAlt, '#FEC85C', DemoLocalizations.of(context).trans('delete_data'), DemoLocalizations.of(context).trans('delete_data_sub')),
 
         ],
       ),
