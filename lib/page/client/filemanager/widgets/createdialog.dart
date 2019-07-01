560a2eeae71b982d3b81b8521ee3c814b4e3492f
rubentalstra <RubenTalstra1211@outlook.com>
2019-07-01 08:13:52 -0400

    yeah let's go iOS

--- lib/page/client/filemanager/widgets/createdialog.dart
+++ lib/page/client/filemanager/widgets/createdialog.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io';
 
 class CreateDialog extends StatelessWidget {
   final TextEditingController controller;
@@ -13,26 +15,39 @@ class CreateDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      title: Text(title),
-      content: Container(
-        child: TextField(
-          autofocus: true,
-          maxLines: 1,
-          autocorrect: false,
-          keyboardType: TextInputType.text,
-          textInputAction: TextInputAction.done,
-        ),
-      ),
-      actions: <Widget>[
-        FlatButton(
-          child: Text("Create"),
-          onPressed: (){
-            Navigator.of(context).pop();
-            onSubmitted();
-          },
-        )
-      ],
-    );
+    return Platform.isIOS
+        ? new CupertinoAlertDialog(
+            title: Text(title),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Create"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  onSubmitted();
+                },
+              )
+            ],
+          )
+        : new AlertDialog(
+            title: Text(title),
+            content: Container(
+              child: TextField(
+                autofocus: true,
+                maxLines: 1,
+                autocorrect: false,
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.done,
+              ),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Create"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  onSubmitted();
+                },
+              )
+            ],
+          );
   }
 }
