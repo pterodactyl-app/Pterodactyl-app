7655c7596cb57cbdbc24965d80cb929ee5d2ec30
Arnaud Lier <zeprofdecoding@gmail.com>
2019-09-06 22:45:37 +0200

    Update app_test.dart

--- test/app_test.dart
+++ test/app_test.dart
@@ -1,8 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:pterodactyl_app/main.dart';
 
 void main() {
-  testWidgets('app should work', (tester) async {
-    expect(true);
-  });
+
 }
