a18615ad662d0e37ad286c0c3c83018bd7bcad66
Arnaud Lier <zeprofdecoding@gmail.com>
2019-09-10 18:07:20 +0200

    Add files via upload

--- android/app/src/main/java/nl/rubentalstra/pterodactyl_app/MainActivity.java
+++ android/app/src/main/java/nl/rubentalstra/pterodactyl_app/MainActivity.java
@@ -1,6 +1,7 @@
 package nl.rubentalstra.pterodactyl_app;
 
 import android.os.Bundle;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
---
+++ android/app/src/main/kotlin/fr/thunderhosting/pterodactyl_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.thunderhosting.pterodactyl_app
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
